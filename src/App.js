be0979d4e490c06f80b0015a83dc804e9776080d
Villain_uni <namo4862@naver.com>
2022-02-06 12:49:50 +0900

    [add] RegisterPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Router, Route} from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Router>
   );
 }
